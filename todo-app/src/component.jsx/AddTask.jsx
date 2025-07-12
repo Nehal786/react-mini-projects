262b4ce2a3358cf11231c21edcc810055966de78
Nehal Ahmed Ansari <nehalahmedansari0101@gmail.com>
2025-07-12 17:10:24 +0530

    added task and displayed todo list

--- todo-app/src/component.jsx/AddTask.jsx
+++ todo-app/src/component.jsx/AddTask.jsx
@@ -1,15 +1,52 @@
-const AddTask = () => {
-    return (
-        <div className="todo-container">
-            <h1>Todo</h1>
-            <div className="todo-input">
-                <input type="checkbox" /> 
-                <input type="text" placeholder="Add task..." />
-            </div>
-            <button className="add-btn">Add</button>
+import { useState } from "react";
 
-        </div>
-    )
-}
+const AddTask = () => {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  function handleTodoList() {
+    if (task.trim() === '') {
+      return;
+    }
+    setTaskList([...taskList, task]);
+    setTask("");
+    console.log(taskList);
+  }
+
+  return (
+    <div className="todo-container">
+      <h1>Todo</h1>
+      <div className="todo-input">
+        <input
+          type="text"
+          placeholder="Add task..."
+          value={task}
+          onChange={(e) => {
+            setTask(e.target.value);
+            // console.log(e.target.value)
+          }}
+        />
+        <button
+          className="add-btn"
+          onClick={() => {
+            handleTodoList();
+          }}
+        >
+          Add
+        </button>
+      </div>
+      <div className="todo-list">
+        <ul className="list">
+          {taskList.map((task, index) => (
+            <li key={index}>
+              <input type="checkbox" />
+              {task}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default AddTask;
