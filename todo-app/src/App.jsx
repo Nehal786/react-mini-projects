72815859f969539e39f6e7616cd0b2c9aa9ac663
Nehal Ahmed Ansari <nehalahmedansari0101@gmail.com>
2025-07-14 18:17:22 +0530

    made counter

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import AddTask from "./component.jsx/AddTask";
+import Counter from "./component.jsx/Counter";
 
 function App() {
   return (
     <>
-      <AddTask/>
+      {/* <AddTask/> */}
+      <Counter/>
     </>
   );
 }
