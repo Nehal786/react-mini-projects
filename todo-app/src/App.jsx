c9ce75ec2eba16ac42452600987fd0dd3ec0ec35
Nehal Ahmed Ansari <nehalahmedansari0101@gmail.com>
2025-07-15 17:08:59 +0530

    image project

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import AddTask from "./component.jsx/AddTask";
+import CatImages from "./component.jsx/CatImages";
 import Counter from "./component.jsx/Counter";
 
 function App() {
   return (
     <>
       {/* <AddTask/> */}
-      <Counter/>
+      {/* <Counter/> */}
+      <CatImages/>
     </>
   );
 }
--- todo-app/src/component.jsx/Counter.jsx
+++ todo-app/src/component.jsx/Counter.jsx
@@ -1,40 +1,55 @@
 import { useState } from "react";
 
 const Counter = () => {
-    const [num,setNum] = useState(1);
-    const[result,setResult] = useState(0);
-    function handleIncrement (){
-        
-        setResult(result + Number(num))
-    }
-    function handleDecrement() {
-        setResult(result - Number(num))
-    }
-    return (
-        <div className="container">
-            <div >{result}</div>
-            <div className="btns">
-                <button onClick={() => {
-                    handleDecrement()
-                }}>-</button>
-                <button onClick={()=> {
-                    handleIncrement();
-                }}>+</button>
-            </div>
-            <div className="increseBy">
-                <span >Increment/Decrement by</span>
-                <input type="number"  className="increment" value={num} onChange={(e) => {
-                    setNum(e.target.value)
-                    // console.log(num)
-                }}/>
-                
-            </div>
-            <button className="resetBtn" onClick={() => {
-                setResult(0)
-            }}>Reset</button>
-            
-        </div>
-    )
-}
+  const [num, setNum] = useState(1);
+  const [result, setResult] = useState(0);
+  function handleIncrement() {
+    setResult(result + Number(num));
+  }
+  function handleDecrement() {
+    setResult(result - Number(num));
+  }
+  return (
+    <div className="container">
+      <div>{result}</div>
+      <div className="btns">
+        <button
+          onClick={() => {
+            handleDecrement();
+          }}
+        >
+          -
+        </button>
+        <button
+          onClick={() => {
+            handleIncrement();
+          }}
+        >
+          +
+        </button>
+      </div>
+      <div className="increseBy">
+        <span>Increment/Decrement by</span>
+        <input
+          type="number"
+          className="increment"
+          value={num}
+          onChange={(e) => {
+            setNum(e.target.value);
+            // console.log(num)
+          }}
+        />
+      </div>
+      <button
+        className="resetBtn"
+        onClick={() => {
+          setResult(0);
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
 
 export default Counter;
