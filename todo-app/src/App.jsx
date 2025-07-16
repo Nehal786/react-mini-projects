9be540b87557e37eb491b68175c6678a016e1edf
Nehal Ahmed Ansari <nehalahmedansari0101@gmail.com>
2025-07-16 20:12:21 +0530

    guess the number project initial commit

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import AddTask from "./component.jsx/AddTask";
 import CatImages from "./component.jsx/CatImages";
 import Counter from "./component.jsx/Counter";
+import GuessTheNumber from "./component.jsx/GuessTheNumber";
 
 function App() {
   return (
     <>
       {/* <AddTask/> */}
       {/* <Counter/> */}
-      <CatImages/>
+      {/* <CatImages/> */}
+      <GuessTheNumber/>
     </>
   );
 }
---
+++ todo-app/src/component.jsx/GuessTheNumber.jsx
@@ -0,0 +1,7 @@
+const GuessTheNumber = () => {
+    return (
+        <div>Hello from guess the number project!</div>
+    )
+}
+
+export default GuessTheNumber;
