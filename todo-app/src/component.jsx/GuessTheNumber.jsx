6e91fd7c51378319a98481c26cf5395f862a385b
Nehal Ahmed Ansari <nehalahmedansari0101@gmail.com>
2025-07-17 19:14:03 +0530

    guessthenumber

--- todo-app/src/component.jsx/GuessTheNumber.jsx
+++ todo-app/src/component.jsx/GuessTheNumber.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react";
+
+ let randomNumber = Math.floor(Math.random() * 100) + 1;
+ 
+console.log('random',randomNumber);
 const GuessTheNumber = () => {
+    const [guessNumber,setGuessNumber] = useState('');
+   
+    function handleCheck() {
+        if(guessNumber.trim() == ''){
+            // console.log("empty")
+            return;
+        }
+        console.log("clicked",typeof(guessNumber),typeof(randomNumber))
+     
+
+    }
     return (
-        <div>Hello from guess the number project!</div>
+        <div className="guess-conatiner">
+            <p>Guess a number between 0 to 100</p>
+            <div className="guess-input">
+                <input type="number" className="inputNum" value={guessNumber} onChange={(e) => {
+                    setGuessNumber(e.target.value);
+                    // console.log(guessNumber);
+                }} />
+            </div>
+            <div className="btns">
+                <button onClick={() => setGuessNumber(0)}>Reset</button>
+                <button onClick={() => {
+                    handleCheck();
+                }}>Check</button>
+            </div>
+            
+        </div>
+        
     )
 }
 
